b64ac8bab2de5a83f70b0991f1da8fc1e8b76505
SilenceTale <chomn55@gmail.com>
2025-01-06 20:13:40 +0900

    Add files via upload

--- src/main/java/com/silencetale/ConfigserverApplication.java
+++ src/main/java/com/silencetale/ConfigserverApplication.java
@@ -2,7 +2,9 @@ package com.silencetale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer // 설정집을 깃에서 가져오도록 어노테이션 사용
 @SpringBootApplication
 public class ConfigserverApplication {
 
